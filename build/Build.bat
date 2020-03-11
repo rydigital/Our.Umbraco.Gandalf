4514fa97fb2f80e009d768b388c73908d59e362d
Anthony <a.dang@ry.com>
2020-03-11 13:17:25 +0000

    Fixed packaging

--- build/Build.bat
+++ build/Build.bat
@@ -1,2 +1,2 @@
-Call nuget.exe restore ..\src\Our.Umbraco.Simple301.sln
+Call nuget.exe restore ..\src\Our.Umbraco.Gandalf.sln
 Call "%programfiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" package.build.xml /bl /p:Configuration=Release

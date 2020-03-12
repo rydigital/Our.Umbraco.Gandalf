ddc58c813de0c706bf0422ccc9ff1efe2f789f7b
Emma burstow <e.burstow@ry.com>
2020-03-12 09:55:19 +0000

    Clearing cache using regex clearbykey

--- src/Our.Umbraco.Gandalf/Core/Services/CachedProxies/AllowedIpServiceCachedProxy.cs
+++ src/Our.Umbraco.Gandalf/Core/Services/CachedProxies/AllowedIpServiceCachedProxy.cs
@@ -4,6 +4,7 @@ using DangEasy.Caching.MemoryCache;
 using Our.Umbraco.Gandalf.Core.Models.DTOs;
 using Our.Umbraco.OpenKeyValue.Core.Models.Pocos;
 using System.Linq;
+using Umbraco.Core.Cache;
 
 namespace Our.Umbraco.Gandalf.Core.Services.CachedProxies
 {
@@ -12,11 +13,13 @@ namespace Our.Umbraco.Gandalf.Core.Services.CachedProxies
 
 		private readonly ICache _cache;
 		private readonly IAllowedIpService _allowedIpService;
+		private readonly IAppCache _runtimeCache;
 
-		public AllowedIpServiceCachedProxy(AllowedIpService allowedIpService, ICache cache)
+		public AllowedIpServiceCachedProxy(AllowedIpService allowedIpService, ICache cache, AppCaches appCaches)
 		{
 			_allowedIpService = allowedIpService;
 			_cache = cache;
+			_runtimeCache = appCaches.RuntimeCache;
 		}
 	
 		public AllowedIpDto Create(string ipAddress, string notes)
@@ -28,11 +31,12 @@ namespace Our.Umbraco.Gandalf.Core.Services.CachedProxies
 		{
 			this._allowedIpService.Delete(id);
 			//need to make this clear the cache
+			_runtimeCache.ClearByKey($"{typeof(AllowedIpServiceCachedProxy)}_(.*)");
 		}
 
 		public IEnumerable<AllowedIpDto> GetAll()
 		{
-			var cacheKey = CacheKey.Build<AllowedIpServiceCachedProxy, IEnumerable<AllowedIpDto>>("-1");
+			var cacheKey = CacheKey.Build<AllowedIpServiceCachedProxy, IEnumerable<AllowedIpDto>>("GetAll");
 			return _cache.Get(cacheKey, () => _allowedIpService.GetAll());
 		}
 
@@ -45,6 +49,7 @@ namespace Our.Umbraco.Gandalf.Core.Services.CachedProxies
 		// as this is getting called in the handler I should cache this one
 		public AllowedIpDto GetByIpAddress(string ipAddress)
 		{
+			var cacheKey = CacheKey.Build<AllowedIpServiceCachedProxy, AllowedIpDto>(ipAddress);
 			return GetAll().FirstOrDefault(a =>a.IpAddress.Equals(ipAddress));
 		}
 
